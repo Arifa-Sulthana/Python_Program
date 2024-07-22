17107c194132e101199eecbe2e11a8594c74d200
Arifa-Sulthana <63820176+Arifa-Sulthana@users.noreply.github.com>
2024-07-22 22:50:40 +0530

    Create brandNameGenerator.py

---
+++ brandNameGenerator.py
@@ -0,0 +1,13 @@
+print("Welcome to Brand generator!!!")
+s = input ("Do you want to generate a brand name?(y/n): ")
+#print(s)
+
+
+if(s == "y"):
+    city = input("What's your city Name: ")
+    petName = input ("What's your pet name: ")
+    mix = city+petName
+    print("The brand name is "+mix)
+    
+else:
+        print("Sad to see you leaving!! hope to meet you again!!")
