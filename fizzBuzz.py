99e840ac63fd01abc692f4c064109c20d81a07a1
Arifa-Sulthana <63820176+Arifa-Sulthana@users.noreply.github.com>
2024-07-27 00:21:23 +0530

    Create fizzBuzz.py

---
+++ fizzBuzz.py
@@ -0,0 +1,22 @@
+# Write your code here 👇
+for i in range(1,101):
+  if(i%3==0 and i%5==0):
+    print("FizzBuzz")
+  elif(i%3==0):
+    print("Fizz")
+  elif(i%5==0):
+    print("Buzz")
+  else:
+    print(i)
+
+# angel'a code:
+# target = 100
+# for number in range(1, target + 1):
+#   if number % 3 == 0 and number % 5 == 0:
+#     print("FizzBuzz")
+#   elif number % 3 == 0:
+#     print("Fizz")
+#   elif number % 5 == 0:
+#     print("Buzz")
+#   else:
+#     print(number)
