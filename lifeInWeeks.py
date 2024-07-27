1fdaf7f4d35a78ccc20434bbacbe9718f815b928
Arifa-Sulthana <63820176+Arifa-Sulthana@users.noreply.github.com>
2024-07-27 23:18:18 +0530

    Create lifeInWeeks.py

---
+++ lifeInWeeks.py
@@ -0,0 +1,8 @@
+age = input()
+# 🚨 Don't change the code above 👆
+# Write your code below this line 👇
+print("the entered age is: "+age)
+A=int(age)
+weeks = (90-A)*52
+# print("You have "+str(count)+" weeks left.")
+print(f"You have {weeks} weeks left.")
